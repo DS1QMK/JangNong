1fc8bbd99c57070298e2ee497484b7f6cb20b26d
JangNong <ds1qmk@gmail.com>
2023-03-30 00:18:55 +0900

    입출력변수

--- 입출력함수.py
+++ 입출력함수.py
@@ -76,4 +76,67 @@ print(variable[6:]) #앞에서 6번째 글짜부터 출력
 
 
 # 문자열 Formatting
+variable = "%d books" % 3
+print(variable)
+# %d는 숫자입력
+
+variable = "%s books" % "3"
+print(variable)
+# %S는 문자 입력
+
+variable = "%s books" % 3
+print(variable)
+# %S는 문자를 받지만 숫자를 넣어도 받는다
+
+variable = "I have {0} {1}".format(3, "books")
+print(variable)
+
+number = 3
+variable = f'I have {number+2} books'
+print(variable)
+
+variable = "I have %d apples and %d books" % (number, number+1)
+print(variable)
+
+variable = "The error rate : %d%%" % 55
+print(variable)
+# The error rate : 55%
+
+
+#문자열 정렬
+variable = "%10s" % "Hello"
+print(variable)
+#전체 문자열의 길이를 10으로 함
+
+variable = "%-10s" % "Hello"
+print(variable)
+# 좌측정렬
+
+variable = "{0:<10}".format("Hello")
+print(variable)
+#왼쪽정렬
+
+variable = "{0:>10}".format("Hello")
+print(variable)
+#오른쪽 정렬
+
+variable = "{0:^10}".format("Hello")
+print(variable)
+#가운데 정렬
+
+variable = "{0:-^10}".format("Hello")
+print(variable)
+#가운데 정렬인데 잘 되었는지 -표시로 채움
+
+variable = "%f" % 3.141592
+print(variable)
+# %f는 실수
+
+variable = "%.4f" % 3.141592
+print(variable)
+# 소수점 자리지정
+
+variable = "%10.4f" % 3.141592
+print(variable)
+# 전체문자는 10자리인데 소수점 4자리
 

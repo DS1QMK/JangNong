368b2344c7674f4811684da51d29296e3cf03308
JangNong <ds1qmk@gmail.com>
2023-03-29 17:08:20 +0900

    입출력함수

--- 입출력함수.py
+++ 입출력함수.py
@@ -1,3 +1,21 @@
 # 입출력함수 연습
 variable = 123
 print(variable)
+
+variable = 1.23E2
+print(variable)
+#지수 10^2
+
+variable = 1.23e3
+print(variable)
+#지수는 대소문자 동일하게 출력
+
+variable = 1.23e-2
+print(variable)
+
+variable = 0x1f
+print(variable)
+
+variable = 0x1F
+print(variable)
+#
